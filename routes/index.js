17ea5e06272016cd502f4d82af618cf1c97b3c0c
GeffreySun <geoffreypadan@gmail.com>
2013-12-20 16:20:15 +0800

    modify index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
  */
 var client = require('./../lib/bus/HttpClient');
 module.exports = function(app){
+    app.get('/', function(req, res){
+        res.redirect('/index.html');
+    });
     app.get('/Default.aspx?*',function(req, res){
         var path = req.url;
         res.writeHead(200, {'Content-Type':'text/xml'});
